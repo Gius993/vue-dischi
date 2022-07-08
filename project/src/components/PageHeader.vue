eb1bf2c1bef4ba3b85342a7291fedbb2fa584329
Gius993 <giuseppevidili9@gmail.com>
2022-07-08 15:36:06 +0200

    header completato

--- project/src/components/PageHeader.vue
+++ project/src/components/PageHeader.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-		
+  <div class="container_my">
+		<div class="logo_container">
+			<img src="../assets/img/icons8-spotify-48.png" alt="logo">
+		</div>
   </div>
 </template>
 
@@ -14,5 +16,14 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 	@import  '../style/variables.scss';
-
+	.logo_container{
+		width: 100px;
+	}
+	.container_my{
+		background-color: $brand_color_a;
+		width: 100%;
+		color: white;
+		font-size: 80px;
+		padding: 10px 40px;
+	}
 </style>
